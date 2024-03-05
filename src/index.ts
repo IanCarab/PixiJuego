d692e2a52c64ce493a97a25551192d31f665096c
Ian Franco Caraballo <74513745+IanCarab@users.noreply.github.com>
2024-03-05 19:16:12 -0300

    AddEye

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application<HTMLCanvasElement>({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +9,19 @@ const app = new Application<HTMLCanvasElement>({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("./eye.jpg");
-console.log(eye.width,eye.height);
-clampy.anchor.set(0.5);
+Loader.shared.add({url: "./eye.jpg", name: "MyEye"});
+Loader.shared.add({url: "./clampy.png", name: "Clampy"});
 
-clampy.x = 250;
-clampy.y = 250;
+Loader.shared.onComplete.add(()=>{
 
-app.stage.addChild(clampy);
+	const clampy: Sprite = Sprite.from("./eye.jpg");
+	console.log(clampy.width,clampy.height);
+	clampy.anchor.set(0.5);
+	
+	clampy.x = 250;
+	clampy.y = 250;
+	
+	app.stage.addChild(clampy);
+
+});
+Loader.shared.load();
