e67c231396baf1d506eb554ea96d9cfb2107534c
Ian Franco Caraballo <74513745+IanCarab@users.noreply.github.com>
2024-03-06 16:39:49 -0300

    fixClampyEyeNotWork

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Loader, Sprite } from 'pixi.js'
+import { Application, Assets, AssetsManifest, Sprite } from 'pixi.js'
 
 const app = new Application<HTMLCanvasElement>({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,19 +9,43 @@ const app = new Application<HTMLCanvasElement>({
 	height: 480
 });
 
-Loader.shared.add({url: "./eye.jpg", name: "MyEye"});
-Loader.shared.add({url: "./clampy.png", name: "Clampy"});
+async function init() {
+	const manifest:AssetsManifest = {
+		bundles: [
+				{
+					name: "Sprites",
+					assets:
+					{
+						"Clampy" : "./clampy.png",
+						"Tim" : "./eye.jgp",
+					}
+				},
+		]
+	}
+	await Assets.init({manifest:manifest});
+	DrawScreen();
+}
 
-Loader.shared.onComplete.add(()=>{
+init();
+async function DrawScreen() {
+	const Sprites = await Assets.loadBundle("Sprites");
 
-	const clampy: Sprite = Sprite.from("./eye.jpg");
-	console.log(clampy.width,clampy.height);
-	clampy.anchor.set(0.5);
-	
-	clampy.x = 250;
-	clampy.y = 250;
+	const clampy: Sprite = Sprite.from(Sprites.Clampy);
+	//const eyesore: Sprite = Sprite.from(Sprites.Tim);
+
+
+	console.log(clampy.width+ " " +clampy.height);
+
+	clampy.anchor.set(0);
+
+	clampy.x = app.screen.width / 4;
+	clampy.y = app.screen.height / 4;
+	clampy.scale.set(0.25);
+
+	/* eyesore.x = app.screen.width / 4;
+	eyesore.y = app.screen.height / 4;
+	eyesore.scale.set(0.25); */
 	
 	app.stage.addChild(clampy);
-
-});
-Loader.shared.load();
+	/* app.stage.addChild(eyesore); */
+}
